5bd823cc24fb729e55f826f0e6e2045e34d162cc
ismail BASSAOUD <bwb-i.bassaoud@fondespierre.com>
2022-06-27 13:59:27 +0000

    initial commit

---
+++ poupe_russe_myscript.js
@@ -0,0 +1,22 @@
+class PoupeeRusse{
+    constructor(dollSize,openStatus){
+        this.dollSize = dollSize;
+        this.openStatus = openStatus;
+}
+
+ouvrir(){
+    ;
+}
+
+fermer(){
+    ;
+}
+
+placerDans(PoupeeRusse, p){
+    ;
+}
+
+sortirDe(PoupeeRusse, p){
+    ;
+}
+}
